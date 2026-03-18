0348df960697ac882ff2425c9f9c38b81281a08e
HaoPhong11 <nguyenhaophong227@gmail.com>
2026-03-19 05:57:27 +0700

    update

--- app/src/main/java/com/example/bookstore/data/dto/request/UserRequest.kt
+++ app/src/main/java/com/example/bookstore/data/dto/request/UserRequest.kt
@@ -3,5 +3,6 @@ package com.example.bookstore.data.dto.request
 data class UserRequest(
     val fullName: String,
     val phoneNumber: String,
-    val address: String
+    val address: String,
+    val email: String
 )
